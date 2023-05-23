d2763305d771090a26a526229c9b5958f494a1b7
chuky200 <fanuso14@hotmail.com>
2023-05-23 20:47:34 -0300

    v000000.1.1
    Seguimos probnado

--- main.js
+++ main.js
@@ -1,16 +1,36 @@
-const tilesProvider = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}';
+
 
 
 let myMap = L.map('map').setView([-34.90603185120515,-56.19126965676146
 ], 15);
 
 
-
-L.tileLayer(tilesProvider,{
+L.Control.geocoder().addTo(myMap); // aqui llamo al plugin geocoder
+var osm = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',{
     maxZoom: 18,    
     attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(myMap)
-L.Control.geocoder().addTo(myMap); // aqui llamo al plugin geocoder
+}).addTo(myMap);
+
+var CartoDB_Voyager = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 20
+});
+var CyclOSM = L.tileLayer('https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png', {
+	maxZoom: 20,
+	attribution: '<a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+
+var baseMaps = {
+    'Vista satelital': osm,
+    'CartoDB_Voyager': CartoDB_Voyager,
+    'CyclOSM': CyclOSM,
+}
+L.control.layers(baseMaps).addTo(myMap);
+L.control.locate().addTo(myMap); 
+
+
 
 
 // Función para agregar un marker dando doble click
